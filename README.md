a3da763fcd2e933066ddca7c291127ed1fb878f3
Buzzertech <prasadnayak1006@gmail.com>
2020-08-15 15:41:53 +0530

    chore: init

---
+++ README.md
@@ -0,0 +1,2 @@
+### Portfolio
+This repository features the code for my personal website which features all of my work and little details about who I am and what I do.
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from 'next/head';
+
+const IndexPage = () => (
+    <>
+        <Head>
+            <title>Home | Buzzertech</title>
+        </Head>
+    </>
+)
+
+export default IndexPage;
