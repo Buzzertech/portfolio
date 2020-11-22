2208d7faa8f4b9a4c0b2b9c5d9f40df936434280
Buzzertech <prasadnayak1006@gmail.com>
2020-11-22 22:27:18 +0530

    fix: minor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { Box, Flex, Heading } from 'rebass';
+import RouteLink from './RouteLink';
 
 const Navbar: FC<{ title?: string | false }> = ({ title }) => (
 	<Flex px={[10, 30, 50]} alignItems="center" height={[50, 100]}>
-		<Heading letterSpacing={-1.5} fontSize="heading" color="text">
-			bzr.
-		</Heading>
+		<RouteLink href="/">
+			<Heading letterSpacing={-1.5} fontSize="heading" color="text">
+				bzr.
+			</Heading>
+		</RouteLink>
 		{!!title && (
 			<>
 				<Box bg="primary" width="3px" height="100%" />
