00ab219cd007b0674874024f26e220ddda9744b1
Buzzertech <prasadnayak1006@gmail.com>
2020-11-24 15:24:28 +0530

    fix: minor

--- src/components/RouteLink.tsx
+++ src/components/RouteLink.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import { Link as LinkComponent, LinkProps } from 'rebass';
 
-const RouteLink: FC<LinkProps & { asAnchor: boolean }> = ({ href, asAnchor, ...props }) => {
+const RouteLink: FC<LinkProps & { asAnchor?: boolean }> = ({ href, asAnchor, ...props }) => {
 	return (
 		<Link href={href as string}>
 			<LinkComponent href={(asAnchor && href) || undefined} {...props} />
